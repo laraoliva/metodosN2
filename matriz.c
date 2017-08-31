cf826450b930000dc16e04093e72564bd1f58b50
laraoliva <laraoliva@id.uff.br>
2017-08-31 19:36:26 -0300

    Declaraçao de varáveis...inicializaçao da matriz

---
+++ matriz.c
@@ -0,0 +1,14 @@
+//PROGRAMA PARA RESOLVER UM SISTEMA LINEAR USANDO O MÉTODO DE GAUSS JORDAN
+
+#include<stdio.h>
+
+int main()
+{
+//#############################Declaraçao de varáveis...inicializaçao da matriz###########################################
+
+     int i,j,k,n=4;
+     float m[4][5]={{1,1,0,3,4},{2,1,-1,1,1},{3,-1,-1,2,-3},{-1,2,3,-1,4}};
+     float s;
+     float x[n];
+     
+}
