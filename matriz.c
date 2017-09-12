4466954a139c93ab571f352c2fc3de45f33cdb2d
laraoliva <laraoliva@id.uff.br>
2017-09-11 21:26:30 -0300

    acrescentei outras variaveis,vetores auxiliares e um vetor para receber os coeficientes

--- matriz.c
+++ matriz.c
@@ -6,16 +6,17 @@ int main()
 {
 //#############################Declaraçao de varáveis...inicializaçao da matriz###########################################
 
-     int i,j,k,n=4;
-     float m[3][4]={{2,2,-1,3},{3,3,1,7},{1,-1,5,5}};
-     float s;
-     float x[n];
+     int i,j,k,a,aux,n=3;
+     int l[n];
+     float m[3][3]={{2,2,-1},{3,3,1},{1,-1,5}};
+     float smax,mult,rmax,s,r;
+     float b[3]={3,7,5},x[n],v[n];
  
 //############################################## Imprimir matriz ########################################################
      printf("\n\n\t\tMATRIZ ESTENDIDA\n\n");
-     for(i=0;i<=3;i++)
+     for(i=0;i<=2;i++)
      {
-        for(j=0;j<=4;j++)         
+        for(j=0;j<=3;j++)         
         {
                printf("\t%0.2f",m[i][j]);
                
@@ -28,12 +29,34 @@ int main()
    
 //########### começar zerando 1 elemento da 2 linha,depois o 2 e Repetir para a segunda coluna, a partir do item 3.########
 
-    for(j=0;j<=3;j++)   //começar pela 1 coluna e assim vai...
+    for(j=0;j<=2;j++)   //começar pela 1 coluna e assim vai...
     {
-        for(i=0;i<=3;i++)          
-        { 
+        for(i=0;i<=2;i++)          
+        {   
+            if(i=j & m[i][j]==0)
+
+            {     if( m[i][j=1]==0)  //muda a linha com linha
+                  {
+                     for(j=0;j<=2;j++)
+                     {   a=m[i][j];
+                         m[i][j]=m[i+1][j]; 
+                         m[i+1][j]=a;
+                     }                    
+                  }
+  
+                  else    //muda coluna com a proxima coluna
+                  {
+                       for(i=0;i<=2;i++)
+                       {  a=m[i][j];
+                          m[i][j]=m[i][j+1]; 
+                          m[i][j+1]=a;
+                       }
+                  }
+            }
+ 
+
             if(i>j)
-            {  
+            {   
                  s=-m[i][j]/m[j][j];
                  for(k=0;k<=n;k++)
                  {
@@ -48,9 +71,9 @@ int main()
 //############################################ Matriz triangular###############################################
 
      printf("\n\n\t\tMATRIZ TRIANGULAR\n\n");
-     for(i=0;i<=3;i++)
+     for(i=0;i<=2;i++)
      {
-        for(j=0;j<=3;j++)         
+        for(j=0;j<=2;j++)         
         {
                printf("\t%0.2f",m[i][j]);
                
@@ -71,7 +94,7 @@ int main()
 //###############################################resultados######################################################
     
     printf("\n\n\t\tRESULTADOS\n\n");
-    for(i=0;i<=3;i++)
+    for(i=0;i<=2;i++)
     {
          printf("\n\tx%d=%0.2f\n",i+1,x[i]);
  
